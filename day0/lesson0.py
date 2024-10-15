646784e6825c36dbcc6ce7883761f6e992b124d3
luka621 <lukagvilava2008@gmail.com>
2024-10-15 08:53:40 -0700

    first commit

---
+++ day0/lesson0.py
@@ -0,0 +1,28 @@
+score = 100
+print("Player's score: " + str(score))
+
+health = 75
+print("Player's health: " + str(health))
+
+attacking_points = 50
+defense_points = 30
+print("The player has " + str(attacking_points) + " attacking points and " + str(defense_points) + " defense points.")
+
+
+math_score = 6
+english_score = 7
+science_score = 8
+chemistry_score = 10
+history_score = 9
+
+print("Scores: Math: " + str(math_score) + 
+      ", English: " + str(english_score) + 
+      ", Science: " + str(science_score) + 
+      ", Chemistry: " + str(chemistry_score) + 
+      ", History: " + str(history_score))
+
+
+name = "John"
+surname = "Doe"
+age = 20
+print("My name is " + name + " " + surname + " and I am " + str(age) + " years old.") 
